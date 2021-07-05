60d8ff96857548733f6f924bbb88781807d8dd3f
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-05 14:57:40 -0500

    Bug Fixes and updates

--- channelHandler.js
+++ channelHandler.js
@@ -1,9 +1,5 @@
 function setChannel(name, icon, url){
 document.getElementById('videoTitle').innerHTML = name;
 document.getElementById('videoLogo').src = '/ChannelLogos/' + icon;
-document.getElementById('videoStream').src = url;
-var video = document.getElementById('my_video_1');
-video.src({
-    src: url
-  });
+
 }
